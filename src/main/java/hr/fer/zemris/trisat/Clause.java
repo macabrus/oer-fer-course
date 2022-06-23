ea4d527889559fb7abbce608fea296457d4b6540
bernard.crnkovic <bernard.crnkovic@fer.hr>
2022-06-23 20:36:07 +0200

    fix

--- src/main/java/hr/fer/zemris/trisat/Clause.java
+++
@@ -1,4 +0,0 @@
-package hr.fer.zemris.trisat;
-
-public class Clause {
-}
--- src/main/java/hr/fer/zemris/trisat/IOptAlgorithm.java
+++
@@ -1,7 +0,0 @@
-package hr.fer.zemris.trisat;
-
-import java.util.Optional;
-
-public interface IOptAlgorithm {
-    Optional<BitVector> solve(Optional<BitVector> initial);
-}
